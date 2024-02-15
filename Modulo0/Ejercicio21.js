27fb1f974889802b05036fa6947f4ee25a114020
Jesica Victora <jesicavictora@gmail.com>
2024-02-15 23:02:53 +0100

    Ejercicio 21

--- Modulo0/Ejercicio21.js
+++ Modulo0/Ejercicio21.js
@@ -1,2 +1,15 @@
-//Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo cuando el resto del numero dividido entre 2 sea 0.
+//Crea un bucle for que vaya desde 0 a 9 y 
+//muestralo por consola solo cuando el resto del numero dividido entre 2 sea 0.
 
+
+
+for(let numero = 0 ; numero<=9 ; numero++){
+
+    if((numero%2) == 0){
+
+        console.log(numero);
+   
+    }
+      
+
+}
