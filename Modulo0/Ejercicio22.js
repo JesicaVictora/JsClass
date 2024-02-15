b35c7c145053346dae36d076ed78d9df26359315
Jesica Victora <jesicavictora@gmail.com>
2024-02-15 23:15:04 +0100

    Ejercicio 22

--- Modulo0/Ejercicio22.js
+++ Modulo0/Ejercicio22.js
@@ -3,3 +3,26 @@
 //Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle y 
 //cambia el mensaje en la última vuelta a 'Dormido!'.
 
+
+for(let oveja = 0 ; oveja<=10 ; oveja++){
+
+    if(oveja<=9){
+
+        console.log('Intentando dormir'); 
+
+    } else {
+
+        console.log('dormido'); 
+
+    }
+ 
+
+}
+
+
+
+
+
+
+
+
